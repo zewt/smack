318151b29e3a6553ded5f1696625c49739994578
Glenn Maynard <glenn@zewt.org>
2011-05-11 14:36:21 -0400

    Add ThreadUtil.uninterruptibleJoin.
    uninterruptibleJoin safely defers interrupts while joining a thread.

---
+++ source/org/jivesoftware/smack/util/ThreadUtil.java
@@ -0,0 +1,48 @@
+/**
+ * $RCSfile$
+ * $Revision: 11616 $
+ * $Date: 2010-02-09 07:40:11 -0500 (Tue, 09 Feb 2010) $
+ *
+ * Copyright 2011 Glenn Maynard
+ *
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jivesoftware.smack.util;
+
+public class ThreadUtil {
+    /**
+     * Join a thread without observing interrupts.
+     * 
+     * If an InterruptedException occurs while joining the thread, it will be deferred
+     * until the join completes.  This allows reliably joining a thread that's expected
+     * to exit immediately without race conditions due to thread interruption.  
+     * 
+     * @param thread The thread to join.
+     */
+    static public void uninterruptibleJoin(Thread thread) {
+        boolean interrupted = false;
+        while(true) {
+            try {
+                thread.join();
+            } catch(InterruptedException e) {
+                // Defer interruptions until we're done.
+                interrupted = true;
+                continue;
+            }
+            break;
+        }
+        if(interrupted)
+            Thread.currentThread().interrupt();
+    }
+};
