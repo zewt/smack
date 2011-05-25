aa0ca6a5328c6c4580745c3b4056e5b068b6e391
Glenn Maynard <glenn@zewt.org>
2011-05-24 23:33:03 -0400

    Fixup SASLHelpersTest.

--- test/org/jivesoftware/smack/sasl/SASLHelpersTest.java
+++ test/org/jivesoftware/smack/sasl/SASLHelpersTest.java
@@ -16,10 +16,6 @@
 
 package org.jivesoftware.smack.sasl;
 
-import java.util.Arrays;
-
-import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.util.StringPrep;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import javax.crypto.Mac;
@@ -32,6 +28,8 @@ public class SASLHelpersTest {
     }
 
     static void assertArrayEquals(int[] expect, byte[] actual) {
+        assertEquals(expect.length, actual.length);
+
         for(int i = 0; i < expect.length; ++i) {
             // Java mysteriously lacks an unsigned byte type, so convert it.
             int val = actual[i];
@@ -52,7 +50,6 @@ public class SASLHelpersTest {
             0x67, 0x1f, 0x54, 0xce, 0x0c, 0x54, 0x0f, 0x78, 0xff, 0xe1, 0xe2, 0x6d, 0xcf, 0x9c, 0x2a, 0x04, 0x7a, 0xea, 0x4f, 0xda
         };
 
-        assertEquals(expect.length, result.length);
         assertArrayEquals(expect, result);
     }
 
@@ -62,6 +59,8 @@ public class SASLHelpersTest {
         int[] expect = new int[] {
             0xa9, 0x4a, 0x8f, 0xe5, 0xcc, 0xb1, 0x9b, 0xa6, 0x1c, 0x4c, 0x08, 0x73, 0xd3, 0x91, 0xe9, 0x87, 0x98, 0x2f, 0xbb, 0xd3
         };
+
+        assertArrayEquals(expect, result);
     }
 
     @Test public void decodeBase64() throws Exception {
